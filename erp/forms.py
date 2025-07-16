a7b9e619c1757ede96e2a92cae63ade49c2831dc
Natchathira <vdnatchathira@gmail.com>
2025-07-16 17:41:21 +0530

    Update forms.py

--- erp/forms.py
+++ erp/forms.py
@@ -2,3 +2,8 @@ class AttendanceForm(forms.ModelForm):
     class Meta:
         model = Attendance
         fields = ['student', 'date', 'period', 'status']
+from django import forms
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['user', 'admission_no', 'dob', 'class_section']
